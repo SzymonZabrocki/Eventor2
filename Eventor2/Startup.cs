16bb9e90c79859400a19b64792f7f02d85254aa0
SzymonZabrocki <56550671+SzymonZabrocki@users.noreply.github.com>
2020-01-21 14:45:31 +0100

    Commit

---
+++ Eventor2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Eventor2.Startup))]
+namespace Eventor2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
